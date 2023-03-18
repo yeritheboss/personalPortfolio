78d778fdb755137ff71530bf725780245c896228
Gerangel <gz.berroteran@gmail.com>
2023-03-18 01:07:31 +0100

    nuevos cambios

--- static/js/main.js
+++ static/js/main.js
@@ -19,7 +19,7 @@ const scrollHeader = () =>{
   this.scrollY >= 50 ? header.classList.add('scroll-header') 
                      : header.classList.remove('scroll-header')
 }
-window.addEventListener('scroll', scrollHeader)
+window.addEventListener('scroll', scrollHeader, {passive: true})
 
 
 const linkWork=document.querySelectorAll('.work__item')
@@ -29,7 +29,7 @@ linkWork.forEach(l=>l.classList.remove('active-work'))
 this.classList.add('active-work')
 }
 
-linkWork.forEach(l=>l.addEventListener('click', activeWork))
+linkWork.forEach(l=>l.addEventListener('click', activeWork, {passive: true}))
 
 
 
@@ -74,16 +74,28 @@ sections.forEach(current =>{
   const sectionHeight = current.offsetHeight,
       sectionTop = current.offsetTop - 70,
       sectionId = current.getAttribute('id'),
-      sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+     // sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+      sectionsClass = document.querySelector('.nav__menu a[href*=\'' + sectionId + '\']')
 
-  if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
-    sectionsClass.classList.add('active-link')
-  }else{
-    sectionsClass.classList.remove('active-link')
-  }                                                    
+      if (sectionsClass) {
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+          sectionsClass.classList.add('active-link')
+        } else {
+          sectionsClass.classList.remove('active-link')
+        }
+      }
+
+     /* if (sectionsClass) {
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+          sectionsClass.classList.add('active-link')
+        } else {
+          sectionsClass.classList.remove('active-link')
+        }
+      }*/
+                                              
 })
 }
-window.addEventListener('scroll', scrollActive)
+window.addEventListener('scroll', scrollActive, {passive: true})
 
 
 /*=============== SERVICES ===============*/
@@ -95,3 +107,35 @@ let  mixerPortfolio = mixitup('.work__container', {
       duration: 300
   }
 });
+
+
+
+/*=============== DARK LIGHT THEME ===============*/ 
+const themeButton = document.getElementById('theme-button')
+const lightTheme = 'light-theme'
+const iconTheme = 'bx-sun'
+
+// Previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+// We obtain the current theme that the interface has by validating the light-theme class
+const getCurrentTheme = () => document.body.classList.contains(lightTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'bx bx-moon' : 'bx bx-sun'
+
+// We validate if the user previously chose a topic
+if (selectedTheme) {
+  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](lightTheme)
+  themeButton.classList[selectedIcon === 'bx bx-moon' ? 'add' : 'remove'](iconTheme)
+}
+
+// Activate / deactivate the theme manually with the button
+themeButton.addEventListener('click', () => {
+    // Add or remove the light / icon theme
+    document.body.classList.toggle(lightTheme)
+    themeButton.classList.toggle(iconTheme)
+    // We save the theme and the current icon that the user chose
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+})
