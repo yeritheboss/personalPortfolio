665dba9720bb6e1fe2b01a96f10e0db6baee22e0
Gerangel <gz.berroteran@gmail.com>
2023-03-20 23:09:10 +0100

    k

--- static/js/ingles.js
+++ static/js/ingles.js
@@ -12,7 +12,7 @@ var typed = new Typed(".texto2", {
 
 var typed2 = new Typed(".description", {
     strings: [
-        "Ingeniero Electrónico de comunicaciones especializado en la informática concretamente en el desarrollo con Java y familiarizado con todas sus herramientas y   frameworks, también tengo conociemientos de front-end como desarrollador web y frameworks de  otros lenguajes. Tengo una gran pasión por el BigData y el mundo de las tecnologias y la  inversión, lo que me llevó a formarme como Data Scientist y como Asesor financiero.",
+        "Electronics and Communications Engineer specialized in computer science, specifically in Java development and familiar with all its tools and frameworks. I also have knowledge of front-end as a web developer and frameworks of other languages. I have a great passion for Big Data and the world of technologies and investments, which led me to train as a Data Scientist and Financial Advisor.",
     ],
     typeSpeed: 50,
     backSpeed: 80,
@@ -20,145 +20,142 @@ var typed2 = new Typed(".description", {
 });
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
-const scrollHeader = () =>{
+const scrollHeader = () => {
     const header = document.getElementById('header')
-    // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
-    this.scrollY >= 50 ? header.classList.add('scroll-header') 
-                       : header.classList.remove('scroll-header')
-  }
-  window.addEventListener('scroll', scrollHeader, {passive: true})
-  
-  
-  const linkWork=document.querySelectorAll('.work__item')
-  
-  function activeWork(){
-  linkWork.forEach(l=>l.classList.remove('active-work'))
-  this.classList.add('active-work')
-  }
-  
-  linkWork.forEach(l=>l.addEventListener('click', activeWork, {passive: true}))
-  
-  
-  
-  
-  
-  
-  
-  const modalViews = document.querySelectorAll('.services__modal'),
-  modalBtns = document.querySelectorAll('.services__button'),
-  modalClose = document.querySelectorAll('.services__modal-close')
-  
-  let modal = function (modalClick) {
-  modalViews[modalClick].classList.add('active-modal')
-  }
-  
-  modalBtns.forEach((mb, i) => {
-  mb.addEventListener('click', () => {
-    modal(i)
-  })
-  
-  })
-  
-  modalClose.forEach((mc) => {
-  mc.addEventListener('click', () => {
-    modalViews.forEach((mv) => {
-        mv.classList.remove('active-modal')
+        // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
+    this.scrollY >= 50 ? header.classList.add('scroll-header') :
+        header.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader, { passive: true })
+
+
+const linkWork = document.querySelectorAll('.work__item')
+
+function activeWork() {
+    linkWork.forEach(l => l.classList.remove('active-work'))
+    this.classList.add('active-work')
+}
+
+linkWork.forEach(l => l.addEventListener('click', activeWork, { passive: true }))
+
+
+
+
+
+
+
+const modalViews = document.querySelectorAll('.services__modal'),
+    modalBtns = document.querySelectorAll('.services__button'),
+    modalClose = document.querySelectorAll('.services__modal-close')
+
+let modal = function(modalClick) {
+    modalViews[modalClick].classList.add('active-modal')
+}
+
+modalBtns.forEach((mb, i) => {
+    mb.addEventListener('click', () => {
+        modal(i)
     })
-  
-  })
-  })
-  
-  
-  
-  
-  /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
-  const sections = document.querySelectorAll('section[id]')
-    
-  const scrollActive = () =>{
+
+})
+
+modalClose.forEach((mc) => {
+    mc.addEventListener('click', () => {
+        modalViews.forEach((mv) => {
+            mv.classList.remove('active-modal')
+        })
+
+    })
+})
+
+
+
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
+
+const scrollActive = () => {
     const scrollY = window.pageYOffset
-  
-  sections.forEach(current =>{
-    const sectionHeight = current.offsetHeight,
-        sectionTop = current.offsetTop - 70,
-        sectionId = current.getAttribute('id'),
-       // sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
-        sectionsClass = document.querySelector('.nav__menu a[href*=\'' + sectionId + '\']')
-  
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight,
+            sectionTop = current.offsetTop - 70,
+            sectionId = current.getAttribute('id'),
+            // sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+            sectionsClass = document.querySelector('.nav__menu a[href*=\'' + sectionId + '\']')
+
         if (sectionsClass) {
-          if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
-            sectionsClass.classList.add('active-link')
-          } else {
-            sectionsClass.classList.remove('active-link')
-          }
+            if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+                sectionsClass.classList.add('active-link')
+            } else {
+                sectionsClass.classList.remove('active-link')
+            }
         }
-  
-       /* if (sectionsClass) {
-          if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
-            sectionsClass.classList.add('active-link')
-          } else {
-            sectionsClass.classList.remove('active-link')
-          }
-        }*/
-                                                
-  })
-  }
-  window.addEventListener('scroll', scrollActive, {passive: true})
-  
-  
-  /*=============== SERVICES ===============*/
-  let  mixerPortfolio = mixitup('.work__container', {
+
+        /* if (sectionsClass) {
+           if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+             sectionsClass.classList.add('active-link')
+           } else {
+             sectionsClass.classList.remove('active-link')
+           }
+         }*/
+
+    })
+}
+window.addEventListener('scroll', scrollActive, { passive: true })
+
+
+/*=============== SERVICES ===============*/
+let mixerPortfolio = mixitup('.work__container', {
     selectors: {
         target: '.work__card'
     },
     animation: {
         duration: 300
     }
-  });
-  
-  
-  
-  /*=============== DARK LIGHT THEME ===============*/ 
-  const themeButton = document.getElementById('theme-button')
-  const lightTheme = 'light-theme'
-  const iconTheme = 'bx-sun'
-  
-  // Previously selected topic (if user selected)
-  const selectedTheme = localStorage.getItem('selected-theme')
-  const selectedIcon = localStorage.getItem('selected-icon')
-  
-  // We obtain the current theme that the interface has by validating the light-theme class
-  const getCurrentTheme = () => document.body.classList.contains(lightTheme) ? 'dark' : 'light'
-  const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'bx bx-moon' : 'bx bx-sun'
-  
-  // We validate if the user previously chose a topic
-  if (selectedTheme) {
+});
+
+
+
+/*=============== DARK LIGHT THEME ===============*/
+const themeButton = document.getElementById('theme-button')
+const lightTheme = 'light-theme'
+const iconTheme = 'bx-sun'
+
+// Previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+// We obtain the current theme that the interface has by validating the light-theme class
+const getCurrentTheme = () => document.body.classList.contains(lightTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'bx bx-moon' : 'bx bx-sun'
+
+// We validate if the user previously chose a topic
+if (selectedTheme) {
     // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
     document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](lightTheme)
     themeButton.classList[selectedIcon === 'bx bx-moon' ? 'add' : 'remove'](iconTheme)
-  }
-  
-  // Activate / deactivate the theme manually with the button
-  themeButton.addEventListener('click', () => {
-      // Add or remove the light / icon theme
-      document.body.classList.toggle(lightTheme)
-      themeButton.classList.toggle(iconTheme)
-      // We save the theme and the current icon that the user chose
-      localStorage.setItem('selected-theme', getCurrentTheme())
-      localStorage.setItem('selected-icon', getCurrentIcon())
-  })
-  
-  /*=====================SCROLL REVEAL ANIMATION======================*/
-  const sr=ScrollReveal({
+}
+
+// Activate / deactivate the theme manually with the button
+themeButton.addEventListener('click', () => {
+    // Add or remove the light / icon theme
+    document.body.classList.toggle(lightTheme)
+    themeButton.classList.toggle(iconTheme)
+        // We save the theme and the current icon that the user chose
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+})
+
+/*=====================SCROLL REVEAL ANIMATION======================*/
+const sr = ScrollReveal({
     origin: 'top',
-    distance:'60px',
-    duration:2500,
-    delay:400,
-  })
-  
-  sr.reveal(`.home__data`)
-  sr.reveal(`.home__handle`, {delay:700})
-  sr.reveal(`.home__img`, {delay:700})
-  sr.reveal(`.home__social, .home__scroll`,{delay:900, origin:'bottom'})
-  
-  
-  
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+})
+
+sr.reveal(`.home__data`)
+sr.reveal(`.home__handle`, { delay: 700 })
+sr.reveal(`.home__img`, { delay: 700 })
+sr.reveal(`.home__social, .home__scroll`, { delay: 900, origin: 'bottom' })
